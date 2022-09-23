b5a6acdc43221fd98af7fa33a90d805420d86346
Alfredo Medina <alfremedpal@gmail.com>
2022-09-23 12:24:48 -0500

    Finish basic footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,9 +1,32 @@
+import Link from 'next/link'
 import styles from './footer.module.scss'
 
 const Footer = () => {
     return (
         <footer className={styles.main}>
-            footer
+            <a
+                href="mailto:info@amedpal.com"
+                target="_blank"
+                rel="noopener"
+            >
+                email
+            </a>
+            &nbsp;|&nbsp;
+            <a
+                href="https://github.com/alfremedpal/"
+                target="_blank"
+                rel="noopener"
+            >
+                github
+            </a>
+            &nbsp;|&nbsp;
+            <a
+                href="https://www.linkedin.com/in/alfredo-medina-874749121/"
+                target="_blank"
+                rel="noopener"
+            >
+                linkedin
+            </a>
         </footer>
     )
 }

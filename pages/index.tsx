9228e576072713cda1dd9496baa6d0c63968d8be
Alfredo Medina <alfremedpal@gmail.com>
2022-09-30 16:33:30 -0500

    Add call to action button in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.scss'
+import Button from '../components/button/button'
 
 const Home: NextPage = () => {
 
@@ -15,20 +16,24 @@ const Home: NextPage = () => {
 	}
 
 	return (
-		<div className={styles.hero}>
-			<div
-				className={styles.heroText}
-				onClick={() => restartAnimation()}
-			>
-				<h1>Alfredo Medina</h1>
-				<p>
-					<span style={{animationName: animationID}}>Cloud Solutions Architect</span>&nbsp;|&nbsp;
-					<span style={{animationName: animationID}}>Team builder</span>&nbsp;|&nbsp;
-					<span style={{animationName: animationID}}>Fullstack developer</span>
-				</p>
+		<>
+			<div className={styles.heroContainer}>
+				<div className={styles.heroText}>
+					<h1>Alfredo Medina</h1>
+					<p onClick={() => restartAnimation()}>
+						<span style={{ animationName: animationID }}>Cloud Solutions Architect</span>&nbsp;|&nbsp;
+						<span style={{ animationName: animationID }}>Team builder</span>&nbsp;|&nbsp;
+						<span style={{ animationName: animationID }}>Fullstack developer</span>
+					</p>
+					<Button
+						href='/contact'
+					>
+						Let's talk
+					</Button>
+				</div>
+
 			</div>
-			
-		</div>
+		</>
 	)
 }
 

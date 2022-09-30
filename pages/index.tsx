ce19dae1264534537e2a86cfe3cfa544a6e01174
Alfredo Medina <alfremedpal@gmail.com>
2022-09-30 13:42:06 -0500

    Finish hero text with animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react'
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
+
+	const [animationID, setAnimationID] = useState<string>(styles.showOff)
+
+	const restartAnimation = () => {
+		if (animationID === styles.showOff) {
+			setAnimationID(styles.showOffRepeat)
+		} else {
+			setAnimationID(styles.showOff)
+		}
+	}
+
 	return (
-		<div>
-			<h1>amedpal.com</h1>
+		<div className={styles.hero}>
+			<div
+				className={styles.heroText}
+				onClick={() => restartAnimation()}
+			>
+				<h1>Alfredo Medina</h1>
+				<p>
+					<span style={{animationName: animationID}}>Cloud Solutions Architect</span>&nbsp;|&nbsp;
+					<span style={{animationName: animationID}}>Team builder</span>&nbsp;|&nbsp;
+					<span style={{animationName: animationID}}>Fullstack developer</span>
+				</p>
+			</div>
+			
 		</div>
 	)
 }

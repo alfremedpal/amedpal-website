dd9642a720323a6a997106dd1609cadfcdc4ee2f
Alfredo Medina <alfremedpal@gmail.com>
2022-10-06 17:42:05 -0500

    Add title and description tags✏️

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <title>Alfredo Medina | Digital Solutions</title>
+                <meta name="description" content="Digital solution design on the cloud, consulting, and full stack development." />
+
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
